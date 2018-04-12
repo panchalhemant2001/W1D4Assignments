e8072330fa4640c0da04ec68fe341bf4524b9cd5
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-12 19:03:13 +0000

    callback map completed

---
+++ callbackmap.js
@@ -0,0 +1,32 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+
+function map(words, getArray) {
+  var result = [];
+
+  words.forEach((element) => {
+    result.push(getArray(element));
+  });
+
+  return result;
+}
+
+
+
+function getLength(word) {
+  return word.length;
+}
+
+
+function getUcase(word) {
+  return word.toUpperCase();
+}
+
+function getLcase(word) {
+  return word.split('').reverse().join('');
+}
+
+
+console.log(map(words, getLength));
+console.log(map(words, getUcase));
+console.log(map(words, getLcase));
