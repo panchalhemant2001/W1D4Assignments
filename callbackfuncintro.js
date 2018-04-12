67e912b0fb328c3afe2f7062173e84a84b8f45cc
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-12 16:35:25 +0000

    Print the index of Waldo when found

---
+++ callbackfuncintro.js
@@ -0,0 +1,26 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+   //Implementing forEach
+
+  /*
+  //way 1
+  arr.forEach(function(element, i) {
+    if (element === "Waldo") {
+      found(i));   // execute callback
+    }
+  });
+  */
+
+  //way2
+  arr.forEach((element, i) => {
+    if (element === "Waldo") {
+      found(i);
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index", index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
